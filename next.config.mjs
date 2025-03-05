e80410ca7c6caadedb6957a841bf1fb3b995fcd3
Burakcan-OK <burakcan.ok@gmail.com>
2025-03-05 18:04:43 +0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
+};
 
 export default nextConfig;
